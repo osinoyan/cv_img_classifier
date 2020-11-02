8556dba9e8bcfbef04a35673b983f138b6b8da67
osinoyan <chopsticks1515@gmail.com>
2020-11-02 20:31:00 +0800

    Checkpoint yo

--- main.py
+++ main.py
@@ -276,6 +276,8 @@ def main_worker(gpu, ngpus_per_node, args):
     # Create train/valid datasets
     train_set = carDataset(root='./data',
                         split='train', transform=train_transform)
+    test_set = carDataset(root='./data',
+                        split='test', transform=train_transform)
     # valid_set = carDataset(root='./data',
     #                     split='valid', transform=valid_transform)
     ########################################################################################################
@@ -291,6 +293,8 @@ def main_worker(gpu, ngpus_per_node, args):
         train_set, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
+    test_loader = DataLoader(test_set, batch_size=args.batch_size,
+        shuffle=False, num_workers=args.workers)
     # val_loader = torch.utils.data.DataLoader(
     #     valid_set),
     #     batch_size=args.batch_size, shuffle=False,
@@ -306,7 +310,7 @@ def main_worker(gpu, ngpus_per_node, args):
         adjust_learning_rate(optimizer, epoch, args)
 
         # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch, args)
+        top1 = train(train_loader, model, criterion, optimizer, epoch, args)
 
         # evaluate on validation set
         # acc1 = validate(val_loader, model, criterion, args)
@@ -314,14 +318,18 @@ def main_worker(gpu, ngpus_per_node, args):
         # remember best acc@1 and save checkpoint
         # is_best = acc1 > best_acc1
         # best_acc1 = max(acc1, best_acc1)
-        is_best = 0
+
+        # remember best acc@1 and save checkpoint
+        is_best = top1 > best_acc1
+        best_acc1 = max(top1, best_acc1)
+        
         if not args.multiprocessing_distributed or (args.multiprocessing_distributed
                 and args.rank % ngpus_per_node == 0):
             save_checkpoint({
                 'epoch': epoch + 1,
                 'arch': args.arch,
                 'state_dict': model.state_dict(),
-                # 'best_acc1': best_acc1,
+                'best_acc1': best_acc1,
                 'optimizer' : optimizer.state_dict(),
             }, is_best)
 
@@ -373,6 +381,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         if i % args.print_freq == 0:
             progress.display(i)
 
+    return top1.avg
 
 def validate(val_loader, model, criterion, args):
     batch_time = AverageMeter('Time', ':6.3f')
@@ -418,6 +427,49 @@ def validate(val_loader, model, criterion, args):
 
     return top1.avg
 
+def runTest(val_loader, model, criterion, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(val_loader),
+        [batch_time, losses, top1, top5],
+        prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            if args.gpu is not None:
+                images = images.cuda(args.gpu, non_blocking=True)
+            if torch.cuda.is_available():
+                target = target.cuda(args.gpu, non_blocking=True)
+
+            # compute output
+            output = model(images)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), images.size(0))
+            top1.update(acc1[0], images.size(0))
+            top5.update(acc5[0], images.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.display(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
